8aa3190d882e00dfc1c5dd4258c64be4285780fa
Bambale0 <83171474+Bambale0@users.noreply.github.com>
2026-07-25 10:48:13 +0300

    feat(bot): add quick-start reference states

--- src/foxgen/bot/states.py
+++ src/foxgen/bot/states.py
@@ -2,6 +2,8 @@ from aiogram.fsm.state import State, StatesGroup
 
 
 class GenerationStates(StatesGroup):
+    quick_start_waiting_media = State()
+    reference_choosing_product = State()
     choosing_mode = State()
     choosing_model = State()
     waiting_prompt = State()
