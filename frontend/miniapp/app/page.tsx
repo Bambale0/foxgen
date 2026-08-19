792b017fd058b04a5e0114372c02bc1c26c6b4a5
Bambale0 <83171474+Bambale0@users.noreply.github.com>
2026-08-19 21:39:19 +0300

    refactor(miniapp): wire state-driven shell

---
+++ frontend/miniapp/app/page.tsx
@@ -0,0 +1,10 @@
+import { MiniAppShell } from '@/components/mini-app-shell'
+import { TabContent } from '@/components/tab-content'
+
+export default function Home() {
+  return (
+    <MiniAppShell>
+      <TabContent />
+    </MiniAppShell>
+  )
+}
