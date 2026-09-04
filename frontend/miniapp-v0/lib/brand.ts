22ff2beb787eef5a0b0abef62431fcf56e6dadc2
Bambale0 <83171474+Bambale0@users.noreply.github.com>
2026-09-04 12:44:12 +0300

    feat(brand): export HappyFox icon

--- frontend/miniapp-v0/lib/brand.ts
+++ frontend/miniapp-v0/lib/brand.ts
@@ -3,3 +3,4 @@ import { PRODUCT } from './product'
 export const BRAND_NAME = PRODUCT.brandName
 export const BRAND_DESCRIPTION = PRODUCT.brandDescription
 export const BRAND_LOGO = PRODUCT.brandLogo
+export const BRAND_ICON = PRODUCT.brandIcon
